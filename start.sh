b9494eed82a99df10bb8c5db41c71d18ff8bae1a
Wany <sung@wany.io>
2023-01-28 04:35:11 +0000

    a

---
+++ start.sh
@@ -0,0 +1,6 @@
+session=action-wanyne
+tmux has-session -t $session || tmux new-session -d -s $session
+tmux send-keys -t $session C-c
+tmux send-keys -t $session "bash" C-m
+tmux send-keys -t $session "cd $PWD" C-m
+tmux send-keys -t $session "npm run start" C-m
